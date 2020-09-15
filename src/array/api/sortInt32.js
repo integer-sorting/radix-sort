4b1323341d2586e3eacfb7976d046427366178f6
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-15 16:05:44 +0200

    :sparkles: feat: Implement sortInt32.

---
+++ src/array/api/sortInt32.js
@@ -0,0 +1,11 @@
+import sortUint32 from './sortUint32';
+
+const sortInt32 = (array) => {
+	const shift = -(2 ** 31);
+	// TODO avoid copying back and forth
+	const data = Array.prototype.map.call(array, (x) => x - shift);
+	const output = sortUint32(data);
+	return Array.prototype.map.call(output, (x) => x + shift);
+};
+
+export default sortInt32;
