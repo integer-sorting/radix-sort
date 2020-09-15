f52b5ad0951714306ae767b4d46597aa06c33da4
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-15 17:46:13 +0200

    Generate docs.

--- script/search_index.js
+++ script/search_index.js
@@ -1,116 +1,134 @@
 window.esdocSearchIndex = [
   [
-    "@aureooms/js-radix-sort/src/array/accumulate.js~accumulate",
+    "@aureooms/js-radix-sort/src/array/core/accumulate.js~accumulate",
     "function/index.html#static-function-accumulate",
-    "<span>accumulate</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/accumulate.js</span>",
+    "<span>accumulate</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/accumulate.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/alloc.js~alloc",
+    "@aureooms/js-radix-sort/src/array/core/alloc.js~alloc",
     "function/index.html#static-function-alloc",
-    "<span>alloc</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/alloc.js</span>",
+    "<span>alloc</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/alloc.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/compose.js~compose",
+    "@aureooms/js-radix-sort/src/array/core/compose.js~compose",
     "function/index.html#static-function-compose",
-    "<span>compose</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/compose.js</span>",
+    "<span>compose</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/compose.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/cumulativehistogram.js~cumulativehistogram",
+    "@aureooms/js-radix-sort/src/array/core/cumulativehistogram.js~cumulativehistogram",
     "function/index.html#static-function-cumulativeHistogram",
-    "<span>cumulativeHistogram</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/cumulativeHistogram.js</span>",
+    "<span>cumulativeHistogram</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/cumulativeHistogram.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/digits.js~digits",
+    "@aureooms/js-radix-sort/src/array/core/digits.js~digits",
     "function/index.html#static-function-digits",
-    "<span>digits</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/digits.js</span>",
+    "<span>digits</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/digits.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/fill.js~fill",
+    "@aureooms/js-radix-sort/src/array/core/fill.js~fill",
     "function/index.html#static-function-fill",
-    "<span>fill</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/fill.js</span>",
+    "<span>fill</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/fill.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/histogram.js~histogram",
+    "@aureooms/js-radix-sort/src/array/core/histogram.js~histogram",
     "function/index.html#static-function-histogram",
-    "<span>histogram</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/histogram.js</span>",
+    "<span>histogram</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/histogram.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/load.js~load",
+    "@aureooms/js-radix-sort/src/array/core/load.js~load",
     "function/index.html#static-function-load",
-    "<span>load</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/load.js</span>",
+    "<span>load</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/load.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/loadlengths.js~loadlengths",
+    "@aureooms/js-radix-sort/src/array/core/loadlengths.js~loadlengths",
     "function/index.html#static-function-loadLengths",
-    "<span>loadLengths</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/loadLengths.js</span>",
+    "<span>loadLengths</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/loadLengths.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/permute.js~permute",
+    "@aureooms/js-radix-sort/src/array/core/permute.js~permute",
     "function/index.html#static-function-permute",
-    "<span>permute</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/permute.js</span>",
+    "<span>permute</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/permute.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/permuteinplace.js~permuteinplace",
+    "@aureooms/js-radix-sort/src/array/core/permuteinplace.js~permuteinplace",
     "function/index.html#static-function-permuteInPlace",
-    "<span>permuteInPlace</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/permuteInPlace.js</span>",
+    "<span>permuteInPlace</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/permuteInPlace.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/rank.js~rank",
+    "@aureooms/js-radix-sort/src/array/core/rank.js~rank",
     "function/index.html#static-function-rank",
-    "<span>rank</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/rank.js</span>",
+    "<span>rank</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/rank.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/reset.js~reset",
+    "@aureooms/js-radix-sort/src/array/core/reset.js~reset",
     "function/index.html#static-function-reset",
-    "<span>reset</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/reset.js</span>",
+    "<span>reset</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/reset.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/sort.js~sort",
+    "@aureooms/js-radix-sort/src/array/api/sort.js~sort",
     "function/index.html#static-function-sort",
-    "<span>sort</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/sort.js</span>",
+    "<span>sort</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/api/sort.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/sortarbitrarytuples.js~sortarbitrarytuples",
+    "@aureooms/js-radix-sort/src/array/core/sortarbitrarytuples.js~sortarbitrarytuples",
     "function/index.html#static-function-sortArbitraryTuples",
-    "<span>sortArbitraryTuples</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/sortArbitraryTuples.js</span>",
+    "<span>sortArbitraryTuples</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/sortArbitraryTuples.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/sortfixedlengthtuples.js~sortfixedlengthtuples",
+    "@aureooms/js-radix-sort/src/array/core/sortfixedlengthtuples.js~sortfixedlengthtuples",
     "function/index.html#static-function-sortFixedLengthTuples",
-    "<span>sortFixedLengthTuples</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/sortFixedLengthTuples.js</span>",
+    "<span>sortFixedLengthTuples</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/sortFixedLengthTuples.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/sorttuplesbylength.js~sorttuplesbylength",
+    "@aureooms/js-radix-sort/src/array/api/sortfloat32.js~sortfloat32",
+    "function/index.html#static-function-sortFloat32",
+    "<span>sortFloat32</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/api/sortFloat32.js</span>",
+    "function"
+  ],
+  [
+    "@aureooms/js-radix-sort/src/array/api/sortint32.js~sortint32",
+    "function/index.html#static-function-sortInt32",
+    "<span>sortInt32</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/api/sortInt32.js</span>",
+    "function"
+  ],
+  [
+    "@aureooms/js-radix-sort/src/array/core/sorttuplesbylength.js~sorttuplesbylength",
     "function/index.html#static-function-sortTuplesByLength",
-    "<span>sortTuplesByLength</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/sortTuplesByLength.js</span>",
+    "<span>sortTuplesByLength</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/sortTuplesByLength.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/stable.js~stable",
+    "@aureooms/js-radix-sort/src/array/api/sortuint32.js~sortuint32",
+    "function/index.html#static-function-sortUint32",
+    "<span>sortUint32</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/api/sortUint32.js</span>",
+    "function"
+  ],
+  [
+    "@aureooms/js-radix-sort/src/array/core/stable.js~stable",
     "function/index.html#static-function-stable",
-    "<span>stable</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/stable.js</span>",
+    "<span>stable</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/stable.js</span>",
     "function"
   ],
   [
-    "@aureooms/js-radix-sort/src/array/zeros.js~zeros",
+    "@aureooms/js-radix-sort/src/array/core/zeros.js~zeros",
     "function/index.html#static-function-zeros",
-    "<span>zeros</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/zeros.js</span>",
+    "<span>zeros</span> <span class=\"search-result-import-path\">@aureooms/js-radix-sort/src/array/core/zeros.js</span>",
     "function"
   ],
   [
@@ -396,117 +414,135 @@ window.esdocSearchIndex = [
     "external"
   ],
   [
-    "src/array/accumulate.js",
-    "file/src/array/accumulate.js.html",
-    "src/array/accumulate.js",
+    "src/array/api/sort.js",
+    "file/src/array/api/sort.js.html",
+    "src/array/api/sort.js",
     "file"
   ],
   [
-    "src/array/alloc.js",
-    "file/src/array/alloc.js.html",
-    "src/array/alloc.js",
+    "src/array/api/sortfloat32.js",
+    "file/src/array/api/sortFloat32.js.html",
+    "src/array/api/sortFloat32.js",
     "file"
   ],
   [
-    "src/array/compose.js",
-    "file/src/array/compose.js.html",
-    "src/array/compose.js",
+    "src/array/api/sortint32.js",
+    "file/src/array/api/sortInt32.js.html",
+    "src/array/api/sortInt32.js",
     "file"
   ],
   [
-    "src/array/cumulativehistogram.js",
-    "file/src/array/cumulativeHistogram.js.html",
-    "src/array/cumulativeHistogram.js",
+    "src/array/api/sortuint32.js",
+    "file/src/array/api/sortUint32.js.html",
+    "src/array/api/sortUint32.js",
     "file"
   ],
   [
-    "src/array/digits.js",
-    "file/src/array/digits.js.html",
-    "src/array/digits.js",
+    "src/array/core/accumulate.js",
+    "file/src/array/core/accumulate.js.html",
+    "src/array/core/accumulate.js",
     "file"
   ],
   [
-    "src/array/fill.js",
-    "file/src/array/fill.js.html",
-    "src/array/fill.js",
+    "src/array/core/alloc.js",
+    "file/src/array/core/alloc.js.html",
+    "src/array/core/alloc.js",
     "file"
   ],
   [
-    "src/array/histogram.js",
-    "file/src/array/histogram.js.html",
-    "src/array/histogram.js",
+    "src/array/core/compose.js",
+    "file/src/array/core/compose.js.html",
+    "src/array/core/compose.js",
     "file"
   ],
   [
-    "src/array/load.js",
-    "file/src/array/load.js.html",
-    "src/array/load.js",
+    "src/array/core/cumulativehistogram.js",
+    "file/src/array/core/cumulativeHistogram.js.html",
+    "src/array/core/cumulativeHistogram.js",
     "file"
   ],
   [
-    "src/array/loadlengths.js",
-    "file/src/array/loadLengths.js.html",
-    "src/array/loadLengths.js",
+    "src/array/core/digits.js",
+    "file/src/array/core/digits.js.html",
+    "src/array/core/digits.js",
     "file"
   ],
   [
-    "src/array/permute.js",
-    "file/src/array/permute.js.html",
-    "src/array/permute.js",
+    "src/array/core/fill.js",
+    "file/src/array/core/fill.js.html",
+    "src/array/core/fill.js",
     "file"
   ],
   [
-    "src/array/permuteinplace.js",
-    "file/src/array/permuteInPlace.js.html",
-    "src/array/permuteInPlace.js",
+    "src/array/core/histogram.js",
+    "file/src/array/core/histogram.js.html",
+    "src/array/core/histogram.js",
     "file"
   ],
   [
-    "src/array/rank.js",
-    "file/src/array/rank.js.html",
-    "src/array/rank.js",
+    "src/array/core/load.js",
+    "file/src/array/core/load.js.html",
+    "src/array/core/load.js",
     "file"
   ],
   [
-    "src/array/reset.js",
-    "file/src/array/reset.js.html",
-    "src/array/reset.js",
+    "src/array/core/loadlengths.js",
+    "file/src/array/core/loadLengths.js.html",
+    "src/array/core/loadLengths.js",
     "file"
   ],
   [
-    "src/array/sort.js",
-    "file/src/array/sort.js.html",
-    "src/array/sort.js",
+    "src/array/core/permute.js",
+    "file/src/array/core/permute.js.html",
+    "src/array/core/permute.js",
     "file"
   ],
   [
-    "src/array/sortarbitrarytuples.js",
-    "file/src/array/sortArbitraryTuples.js.html",
-    "src/array/sortArbitraryTuples.js",
+    "src/array/core/permuteinplace.js",
+    "file/src/array/core/permuteInPlace.js.html",
+    "src/array/core/permuteInPlace.js",
     "file"
   ],
   [
-    "src/array/sortfixedlengthtuples.js",
-    "file/src/array/sortFixedLengthTuples.js.html",
-    "src/array/sortFixedLengthTuples.js",
+    "src/array/core/rank.js",
+    "file/src/array/core/rank.js.html",
+    "src/array/core/rank.js",
     "file"
   ],
   [
-    "src/array/sorttuplesbylength.js",
-    "file/src/array/sortTuplesByLength.js.html",
-    "src/array/sortTuplesByLength.js",
+    "src/array/core/reset.js",
+    "file/src/array/core/reset.js.html",
+    "src/array/core/reset.js",
     "file"
   ],
   [
-    "src/array/stable.js",
-    "file/src/array/stable.js.html",
-    "src/array/stable.js",
+    "src/array/core/sortarbitrarytuples.js",
+    "file/src/array/core/sortArbitraryTuples.js.html",
+    "src/array/core/sortArbitraryTuples.js",
     "file"
   ],
   [
-    "src/array/zeros.js",
-    "file/src/array/zeros.js.html",
-    "src/array/zeros.js",
+    "src/array/core/sortfixedlengthtuples.js",
+    "file/src/array/core/sortFixedLengthTuples.js.html",
+    "src/array/core/sortFixedLengthTuples.js",
+    "file"
+  ],
+  [
+    "src/array/core/sorttuplesbylength.js",
+    "file/src/array/core/sortTuplesByLength.js.html",
+    "src/array/core/sortTuplesByLength.js",
+    "file"
+  ],
+  [
+    "src/array/core/stable.js",
+    "file/src/array/core/stable.js.html",
+    "src/array/core/stable.js",
+    "file"
+  ],
+  [
+    "src/array/core/zeros.js",
+    "file/src/array/core/zeros.js.html",
+    "src/array/core/zeros.js",
     "file"
   ],
   [
