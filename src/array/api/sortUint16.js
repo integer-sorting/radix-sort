4d636bcea084dabffe1f386079b445ea23ae338e
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-16 16:22:56 +0200

    :sparkles: feat(sortUint16): First draft.
    Fixes #7.

---
+++ src/array/api/sortUint16.js
@@ -0,0 +1,15 @@
+import sort from './sort';
+
+const sortUint16 = (array) => {
+	const k = 2; // TODO make it depend on array.length
+	const M = 2 ** 8;
+	// TODO avoid copying back and forth
+	const tuples = Array.prototype.map.call(array, (x) => [
+		(x & 0xff00) >>> 8,
+		(x & 0xff) >>> 0
+	]);
+	const output = sort(k, M, tuples);
+	return Array.prototype.map.call(output, ([c, d]) => ((c << 8) | d) >>> 0);
+};
+
+export default sortUint16;
