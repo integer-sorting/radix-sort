298b8fb274bc9c9fb11447a4202588a9188b6ee3
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-09-16 16:07:11 +0200

    :sparkles: feat(sortInt8): First draft.
    Fixes #8.

---
+++ src/array/api/sortInt8.js
@@ -0,0 +1,11 @@
+import sortUint8 from './sortUint8';
+
+const sortInt8 = (array) => {
+	const shift = -(2 ** 7);
+	// TODO avoid copying back and forth
+	const data = Array.prototype.map.call(array, (x) => x - shift);
+	const output = sortUint8(data);
+	return Array.prototype.map.call(output, (x) => x + shift);
+};
+
+export default sortInt8;
---
+++ test/src/api/sortInt8.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import {sorted, range, list, map} from '@aureooms/js-itertools';
+import {shuffle, randrange} from '@aureooms/js-random';
+import {increasing} from '@aureooms/js-compare';
+
+import sortInt8 from '../../../src/array/api/sortInt8';
+
+const macro = (t, data) => {
+	const result = sortInt8(data.slice());
+	const expected = sorted(increasing, data);
+	t.deepEqual(expected, result);
+};
+
+const repr = (data) =>
+	data.length >= 20
+		? `[${data.slice(0, 9)},..,${data.slice(-9)}]`
+		: JSON.stringify(data);
+
+macro.title = (title, data) => title || `sortInt8(${repr(data)})`;
+
+test(macro, []);
+test(macro, [1, -2, -3, 4]);
+
+const N = 1 << 17;
+const longShuffledInput = list(
+	Int8Array.from(range((-N / 2) | 0, (N / 2) | 0))
+);
+shuffle(longShuffledInput, 0, N);
+test(macro, longShuffledInput);
+
+const longRandomInput = list(map(() => randrange(-(2 ** 7), 2 ** 7), range(N)));
+test(macro, longRandomInput);
