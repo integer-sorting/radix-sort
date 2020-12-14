503941248bc4c7e1d41b63dc9568903be38a2a33
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-12-14 23:21:36 +0100

    :sparkles: feat: Export API.

--- src/array/api/index.js
+++ src/array/api/index.js
@@ -1,13 +1,11 @@
-import {
-	sort,
-	sortFloat32,
-	sortInt16,
-	sortInt32,
-	sortInt8,
-	sortUint16,
-	sortUint32,
-	sortUint8
-} from './array/api';
+import sort from './sort';
+import sortFloat32 from './sortFloat32';
+import sortInt16 from './sortInt16';
+import sortInt32 from './sortInt32';
+import sortInt8 from './sortInt8';
+import sortUint16 from './sortUint16';
+import sortUint32 from './sortUint32';
+import sortUint8 from './sortUint8';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
