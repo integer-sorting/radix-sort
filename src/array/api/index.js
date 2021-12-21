69171154c5efe41bf04c6a5c149ed44052a23cb8
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-12-21 13:54:26 +0100

    :recycle: refactor(index.js): Simplify.

--- src/array/api/index.js
+++
@@ -1,8 +0,0 @@
-export {default as sort} from './sort.js';
-export {default as sortInt16} from './sortInt16.js';
-export {default as sortFloat32} from './sortFloat32.js';
-export {default as sortInt8} from './sortInt8.js';
-export {default as sortInt32} from './sortInt32.js';
-export {default as sortUint32} from './sortUint32.js';
-export {default as sortUint16} from './sortUint16.js';
-export {default as sortUint8} from './sortUint8.js';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-export {
-	sort,
-	sortInt16,
-	sortFloat32,
-	sortInt8,
-	sortInt32,
-	sortUint16,
-	sortUint8,
-	sortUint32,
-} from './array/api/index.js';
+export {default as sort} from './array/api/sort.js';
+export {default as sortInt16} from './array/api/sortInt16.js';
+export {default as sortFloat32} from './array/api/sortFloat32.js';
+export {default as sortInt8} from './array/api/sortInt8.js';
+export {default as sortInt32} from './array/api/sortInt32.js';
+export {default as sortUint32} from './array/api/sortUint32.js';
+export {default as sortUint16} from './array/api/sortUint16.js';
+export {default as sortUint8} from './array/api/sortUint8.js';
